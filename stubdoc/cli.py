5cdd75c4003cc2e0d8c7676ec426c42d9404515d
simon-ritchie <antisocial.sid2@gmail.com>
2020-12-16 21:17:11 +0900

    Replace argument setting by class and constant.

--- stubdoc/cli.py
+++ stubdoc/cli.py
@@ -1,12 +1,93 @@
 """The module that handles command line interface implementations.
 """
 
-from typing import Optional
+from typing import List, Optional
 import argparse
+from argparse import ArgumentParser
+from argparse import Namespace
 
 _DESCRIPTION: str = (
     'This command will add docstring to stub file.'
 )
+
+
+class Arg:
+
+    short_name: str
+    long_name: str
+    type_: type
+    help: str
+
+    def __init__(
+            self, short_name: str, long_name: str, type_: type,
+            help: str) -> None:
+        """
+        The class that store single argument setting.
+
+        Parameters
+        ----------
+        short_name : str
+            Argument short name. e.g., '-s'
+            Necessary to start with single hyphen.
+        long_name : str
+            Argument long name. e.g., '--sample_arg'
+            Necessary to start with double hyphen.
+        type_ : type
+            Argument type. e.g., str, int, etc.
+        help : str
+            The argument's help text.
+        """
+        self.short_name = short_name
+        self.long_name = long_name
+        self.type_ = type_
+        self.help = help
+
+
+def _add_arg(
+        parser: ArgumentParser, arg: Arg) -> None:
+    """
+    Add argument to argument parser.
+
+    Parameters
+    ----------
+    parser : ArgumentParser
+        The parser to add argument.
+    arg: Arg
+        Single argument to add to the parser.
+
+    Raises
+    ------
+    ValueError
+        - If short_name not starts with single hyphen.
+        - If long_name not starts with double hyphen.
+    """
+    if arg.short_name.startswith('--'):
+        raise ValueError(
+            'Double hyphen not allowed for short_name argument: '
+            f'{arg.short_name}')
+    if not arg.short_name.startswith('-'):
+        raise ValueError(
+            'short_name is not starts with single hyphen: '
+            f'{arg.short_name}')
+    if not arg.long_name.startswith('--'):
+        raise ValueError(
+            'long_name is not starts with double hyphen: '
+            f'{arg.long_name}')
+
+    parser.add_argument(
+        arg.short_name, arg.long_name, type=arg.type_, help=arg.help)
+
+
+ARGS: List[Arg] = [
+    Arg(short_name='-m',
+        long_name='--module_path',
+        type_=str,
+        help='Stub file\'s original module path. e.g., sample/path.py'),
+    Arg(short_name='-s',
+        long_name='--stub_path',
+        type_=str,
+        help='Target stub file path. e.g., sample/path.pyi'),
+]
 
 
 def main():
@@ -18,18 +99,12 @@ def main():
     args : Namespace or None, default None
         instance that stores arguments data. Only specified None at testing.
     """
-    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+    parser: ArgumentParser = argparse.ArgumentParser(
         description=_DESCRIPTION)
 
-    parser.add_argument(
-        '-m', '--module_path', type=str,
-        help='Stub file\'s original module path. e.g., sample/path.py',
-    )
-    parser.add_argument(
-        '-s', '--stub_path', type=str,
-        help='Target stub file path. e.g., sample/path.pyi',
-    )
-    args: argparse.Namespace = parser.parse_args()
+    for arg in ARGS:
+        _add_arg(parser=parser, arg=arg)
+    args: Namespace = parser.parse_args()
     print('module_path' , args.module_path)
     print('stub_path', args.stub_path)
 
---
+++ tests/test_cli.py
@@ -0,0 +1,49 @@
+from argparse import ArgumentParser
+from argparse import Namespace
+
+import pytest
+
+from stubdoc import cli
+
+
+def test__add_arg() -> None:
+    parser = ArgumentParser()
+
+    with pytest.raises(ValueError):  # type: ignore
+        cli._add_arg(
+            parser=parser,
+            arg=cli.Arg(
+                short_name='--test_arg',
+                long_name='--test_arg',
+                type_=str,
+                help='test help.'
+            ))
+
+    with pytest.raises(ValueError):  # type: ignore
+        cli._add_arg(
+            parser=parser,
+            arg=cli.Arg(
+                short_name='test_arg',
+                long_name='--test_arg',
+                type_=str,
+                help='test help.'
+            ))
+
+    with pytest.raises(ValueError):  # type: ignore
+        cli._add_arg(
+            parser=parser,
+            arg=cli.Arg(
+                short_name='-t',
+                long_name='-test_arg',
+                type_=str,
+                help='test help.'
+            ))
+
+    cli._add_arg(
+        parser=parser,
+        arg=cli.Arg(
+            short_name='-t',
+            long_name='--test_arg',
+            type_=str,
+            help='test help.'
+        ))
