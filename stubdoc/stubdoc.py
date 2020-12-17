e1265089dd4afd9683cd296de86e5845e36e7f6c
simon-ritchie <antisocial.sid2@gmail.com>
2020-12-17 21:42:55 +0900

    Add helper function to read module.

--- stubdoc/stubdoc.py
+++ stubdoc/stubdoc.py
@@ -1,6 +1,12 @@
 """The module that implements core functions.
 """
 
+import sys
+import importlib
+from types import ModuleType
+
+from typing import List
+
 
 def add_docstring_to_stubfile(
         original_module_path: str, stub_file_path: str) -> None:
@@ -14,8 +20,56 @@ def add_docstring_to_stubfile(
     stub_file_path : str
         Target stub file path.
     """
+    module = _read_module(module_path=original_module_path)
     module_str: str = _read_txt(file_path=original_module_path)
+    stub_str: str = _read_txt(file_path=stub_file_path)
+    callable_names: List[str] = _get_callable_names_from_module(
+        module_str=module_str)
+
+
+def _read_module(module_path: str) -> ModuleType:
+    """
+    Read specified path's module.
+
+    Parameters
+    ----------
+    module_path : str
+        Target module path to read.
+
+    Returns
+    -------
+    module : ModuleType
+        Read module.
+    """
+    name: str = module_path.replace('.py', '')
+    name = name.replace('/', '.')
+    name = name.replace('\\', '.')
+    while name.startswith('.'):
+        name = name.replace('.', '', 1)
+    module: ModuleType = importlib.import_module(name)
+    return module
+
+
+def _get_callable_names_from_module(module_str: str) -> List[str]:
+    """
+    Get callable names in the module string.
+
+    Parameters
+    ----------
+    module_str : str
+        Target module string.
+
+    Returns
+    -------
+    callable_names : list of str
+        Result callable names in module str.
+        If class method or nested function, name will be concatenated
+        by comma.
+        e.g., `_read_txt`, `SampleClass._read_text`,
+        `any_function.nested_function`.
+    """
     pass
+
 
 
 def _read_txt(file_path: str) -> str:
--- tests/test_stubdoc.py
+++ tests/test_stubdoc.py
@@ -1,3 +1,5 @@
+from types import ModuleType
+
 import pytest
 
 from stubdoc import stubdoc
@@ -7,3 +9,13 @@ def test__read_txt() -> None:
     txt: str = stubdoc._read_txt(
         file_path='stubdoc/__init__.py')
     assert '__version__' in txt
+
+
+def test__read_module() -> None:
+    module: ModuleType = stubdoc._read_module(
+        module_path='./stubdoc/stubdoc.py')
+    assert module == stubdoc
+
+    module = stubdoc._read_module(
+        module_path='.\\stubdoc\\stubdoc.py')
+    assert module == stubdoc
