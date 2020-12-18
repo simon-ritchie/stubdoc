4d4a21479f39e32e52355fb1a9606c1e05795448
simon-ritchie <antisocial.sid2@gmail.com>
2020-12-18 20:57:39 +0900

    Add function to get callable object names.

--- stubdoc/stubdoc.py
+++ stubdoc/stubdoc.py
@@ -1,17 +1,23 @@
 """The module that implements core functions.
 """
 
+import inspect
 import sys
 import importlib
 from types import ModuleType
 
-from typing import List
+from typing import Any, Callable, List, Tuple
 
 
 def add_docstring_to_stubfile(
         original_module_path: str, stub_file_path: str) -> None:
     """
     Add docstring to specified stub file.
+
+    Notes
+    -----
+    Currently only applied top level function or top level class
+    methods. Not to be applied to nested function.
 
     Parameters
     ----------
@@ -24,7 +30,7 @@ def add_docstring_to_stubfile(
     module_str: str = _read_txt(file_path=original_module_path)
     stub_str: str = _read_txt(file_path=stub_file_path)
     callable_names: List[str] = _get_callable_names_from_module(
-        module_str=module_str)
+        module=module)
 
 
 def _read_module(module_path: str) -> ModuleType:
@@ -50,26 +56,68 @@ def _read_module(module_path: str) -> ModuleType:
     return module
 
 
-def _get_callable_names_from_module(module_str: str) -> List[str]:
+def _get_callable_names_from_module(module: ModuleType) -> List[str]:
     """
-    Get callable names in the module string.
+    Get callable names defined in specified module.
 
     Parameters
     ----------
-    module_str : str
-        Target module string.
+    module : ModuleType
+        Target module.
 
     Returns
     -------
     callable_names : list of str
         Result callable names in module str.
-        If class method or nested function, name will be concatenated
-        by comma.
-        e.g., `_read_txt`, `SampleClass._read_text`,
-        `any_function.nested_function`.
+        If class method exists, name will be concatenated by comma.
+        e.g., `_read_txt`, `SampleClass._read_text`.
+        Nested function will not be included.
     """
-    pass
+    callable_names: List[str] = []
+    members: List[Tuple[str, Any]] = inspect.getmembers(module)
+    for member_name, member_val in members:
+        if inspect.isfunction(member_val):
+            callable_names.append(member_name)
+            continue
+        if inspect.isclass(member_val):
+            _append_class_callable_names_to_list(
+                callable_names= callable_names,
+                class_name=member_name,
+                class_val=member_val)
+            continue
+    return callable_names
 
+
+def _append_class_callable_names_to_list(
+        callable_names: List[str], class_name: str,
+        class_val: type) -> None:
+    """
+    Append class's member method names to list.
+    Name will be added as following format:
+    <class_name>.<method_name>
+
+    Parameters
+    ----------
+    callable_names : list of str
+        The list that append names to.
+    class_name : str
+        Target Class name.
+    class_val : type
+        Target class.
+    """
+    members: List[Tuple[str, Any]] = inspect.getmembers(
+        class_val,
+    )
+    for member_name, member_val in members:
+        if (not isinstance(member_val, Callable)
+                and not isinstance(member_val, property)):
+            continue
+        if (member_name.startswith('__') and member_name != '__init__'):
+            continue
+        if inspect.isclass(member_val):
+            continue
+        name: str = f'{class_name}.{member_name}'
+        callable_names.append(name)
 
 
 def _read_txt(file_path: str) -> str:
--- tests/test_stubdoc.py
+++ tests/test_stubdoc.py
@@ -1,4 +1,6 @@
 from types import ModuleType
+from typing import List
+import sys
 
 import pytest
 
@@ -19,3 +21,53 @@ def test__read_module() -> None:
     module = stubdoc._read_module(
         module_path='.\\stubdoc\\stubdoc.py')
     assert module == stubdoc
+
+
+class _TestClass1:
+
+    test_val: int = 100
+
+    def __init__(self):
+        ...
+
+    def test_method(self):
+        ...
+
+    @property
+    def test_property(self) -> int:
+        return 5
+
+    def __eq__(self, other) -> bool:
+        return True
+
+    class _TestClass2:
+        ...
+
+
+test_value: list = []
+
+
+def test__append_class_callable_names_to_list() -> None:
+
+    callable_names: List[str] = []
+    stubdoc._append_class_callable_names_to_list(
+        callable_names= callable_names,
+        class_name='_TestClass1',
+        class_val=_TestClass1)
+    assert sorted(callable_names) == \
+        sorted([
+            '_TestClass1.__init__',
+            '_TestClass1.test_method',
+            '_TestClass1.test_property'])
+
+    print(callable_names)
+
+
+def test__get_callable_names_from_module():
+    this_module: ModuleType = sys.modules[__name__]
+    callable_names: List[str] = stubdoc._get_callable_names_from_module(
+        module=this_module,
+    )
+    assert 'test__get_callable_names_from_module' in callable_names
+    assert '_TestClass1.__init__' in callable_names
+    assert 'test_value' not in callable_names
